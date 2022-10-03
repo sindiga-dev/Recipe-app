bf2d97f0561227381dc62aed506666a7fbc60825
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-03 22:40:53 +0300

    Initiliazed the bar

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -14,10 +14,51 @@ const Form = () => {
     };
 
     useEffect(() => {
-        const fetchItems = async () => {
+        const getItems = async () => {
+            try{
             const result = await fetch( `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
-            const data = await result.json();
-            setItems(data.items);
+            if (Response.ok) {
+                const data = await result.json();
+                setItems(data.meals);
+                console.log(data.meals);
+            }
+            else {
+                throw new Error("Ooops Request Failed");
+            }
+        } catch (error) {
+            console.log(error);
+        }
         };
-        fetchItems();
-    }
+
+        getItems();
+    }, [query]);
+    return(
+        <div className={styles.wrapper}>
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Search for a meal"
+                    onChange={(e) => setInputValue(e.target.value)}
+                    value={inputValue}
+                />
+                <input type="submit" value="Search" />
+            </form>
+            
+            <div className={styles.container}>
+                {items.map((item) => {
+                return (
+                    <Item
+                    key={item.idMeal}
+                    title={item.strMeal}
+                    image={item.strMealThumb}
+                    category={item.strCategory}
+                    instructions={item.strInstructions}
+                    />
+                );
+            })}
+            </div>
+        </div>
+    );
+};
+
+export default Form;
