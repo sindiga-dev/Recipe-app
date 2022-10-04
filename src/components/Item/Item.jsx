28c71c17455e2e92164aab0f09f41a861156ab3c
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-04 08:32:18 +0300

    Adds view menu button

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,18 +1,23 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styles from "./Item.module.css";
-import Mode from "../../components/Mode/Mode";
+import Modal from "../../components/Modal/Modal";
 
-const manuItem=({image, title, category, instructions}) => {
-    const [show, setShow] = useState(false);
-    return(
-        <div className={styles.card}>
-            <h3 className={styles.title}>Name: {title}</h3>
-                <img className={styles.image} src={image} alt={meal} />
-                <p>category: {category}</p>
-                <button onClick={() => setShow(true)}>View recipe</button>
-                
-                <Mode />
-            </div>
-        </div>
-    );
-}
+const Item = ({ image, title, category, instructions }) => {
+  const [show, setShow] = useState(false);
+  return (
+    <div className={styles.card}>
+      <h3 className={styles.title}>Name: {title}</h3>
+      <img className={styles.image} src={image} alt="meal" />
+      <p>Category: {category}</p>
+      <button onClick={() => setShow(true)}>View recipe</button>
+      <Modal
+        show={show}
+        onClose={() => setShow(false)}
+        modalImg={image}
+        modalTitle={title}
+        modalInstructions={instructions}
+      />
+    </div>
+  );
+};
+export default Item;
