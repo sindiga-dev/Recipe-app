80bb2f3fffb26c3fb44469d29c4d0a4653dedb7e
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-04 08:47:20 +0300

    Debugged the error

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,23 +1,27 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import styles from "./Item.module.css";
-import Modal from "../../components/Modal/Modal";
+import Mode from "../../components/Mode/Mode";
 
-const Item = ({ image, title, category, instructions }) => {
-  const [show, setShow] = useState(false);
-  return (
-    <div className={styles.card}>
-      <h3 className={styles.title}>Name: {title}</h3>
-      <img className={styles.image} src={image} alt="meal" />
-      <p>Category: {category}</p>
-      <button onClick={() => setShow(true)}>View recipe</button>
-      <Modal
-        show={show}
-        onClose={() => setShow(false)}
-        modalImg={image}
-        modalTitle={title}
-        modalInstructions={instructions}
-      />
-    </div>
-  );
+const MenuItem=({image, title, category, instructions}) => {
+    
+    const [show, setShow] = useState(false);
+    return(
+        <div className={styles.card}>
+            <h3 className={styles.title}>Name: {title}</h3>
+                <img className={styles.image} src={image} alt="meal" />
+                <p>category: {category}</p>
+                <button onClick={() => setShow(true)}>View recipe</button>
+                <Mode
+                    show={show}
+                    onClose={() => setShow(false)}
+                    modalImg={image}
+                    modalTitle={title}
+                    modalInstructions={instructions}
+                />
+        </div>
+        
+    );
 };
-export default Item;
+//Debugged line 7: const MenuItem=({image, title, category, instructions}) => //had names my app in small letters
+
+export default MenuItem;
